216c5e376947193ed6fb32517d8c52babd175695
thuylinh13579 <linhnt.1440101226614@vtc.edu.vn>
2023-11-16 19:38:49 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Route::get('/', function () {
 });
 Route::get('/index',[PageController::class, 'getIndex'])->name('index');
 Route::get('/login',[PageController::class, 'getIndexLogin'])->name('login');
-Route::post('/login',[PageController::class, 'postIndexLogin'])->name('login');
+
 Route::get('/createAccount',[PageController::class, 'getIndexCreateAccount'])->name('createAccount');
